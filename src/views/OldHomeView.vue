08d13d155701f6065d84e4eccf5a41a62f73463a
Makayla <makaylabusher@gmail.com>
2023-01-26 12:38:25 -0600

    move vue practice to OldHomeView.vue

---
+++ src/views/OldHomeView.vue
@@ -0,0 +1,31 @@
+/* eslint-disable */
+<script>
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      year: "2023",
+      location: "Chicago",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo3EwEdzdUs6GtLmGyFLcClr-UipWleLJApA&usqp=CAU",
+      count: 0
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h3>{{ year }}</h3>
+    <img v-bind:src="image" />
+    <p><button v-on:click="count++">Add Count</button></p>
+    <p>Count is: {{ count }}</p>
+    <p>{{ location }}</p>
+    Location: <input type="text" v-model="location" />
+  </div>
+</template>
+
+<style></style>
