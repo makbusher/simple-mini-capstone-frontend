f9c6ba3965585a3130320f89eae5a1b989243caf
Makayla <makaylabusher@gmail.com>
2023-01-26 14:33:15 -0600

    add new product action, need to debug

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,8 @@ export default {
       message: "Welcome to Vue.js!",
       year: "2023",
       location: "Chicago",
-      products: []
+      products: [],
+      newProductParams: {}
     };
   },
   created: function() {
@@ -20,8 +21,18 @@ export default {
         this.products = response.data
         console.log(response.data)
       })
-    }
-  }
+    },
+    createProduct: function() {
+      axios.post("http:localhost:3000/products.json", this.newProductParams).then((response) => {
+        console.log("products create", response);
+        this.products.push(response.data);
+        this.newProductParams = {};
+      })
+      .catch((error) => {
+        console.log("products create error", error.response);
+      });
+    },
+  },
 };
 </script>
 
@@ -30,6 +41,21 @@ export default {
     <h1>{{ message }}</h1>
     <h3>{{ year }}</h3>
     <p><button v-on:click="getData">Data</button></p>
+    <h1>New Product</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newProductParams.name" />
+      Price:
+      <input type="text" v-model="newProductParams.price" />
+      Description:
+      <input type="text" v-model="newProductParams.description" />
+      Inventory:
+      <input type="text" v-model="newProductParams.inventory" />
+      Supplier ID:
+      <input type="text" v-model="newProductParams.supplier_id" />
+      <button v-on:click="createProduct()">Create Product</button>
+    </div>
+    <h2>All Products</h2>
     <div v-for="product in products" v-bind:key="product.id">
       <h3>{{ product.name }}</h3>
       <p> {{ product.price }}</p>
