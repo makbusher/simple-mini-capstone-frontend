af64ccbc60864c07b15dae4ce5029779b7ff1623
Makayla <makaylabusher@gmail.com>
2023-01-26 11:43:45 -0600

    init + replace HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
+/* eslint-disable */
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      year: "2023"
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h3>{{ year }}</h3>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
+<style></style>
